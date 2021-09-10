a320f44ed14cbb2a78979906b4c118015869a219
yangjing <2724843654@qq.com>
2021-09-10 17:14:51 +0800

    合并

--- src/main/java/com/example/vehicle_networking/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/vehicle_networking/service/impl/VehicleServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author hgp
